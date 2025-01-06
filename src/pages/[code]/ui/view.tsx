4f4018f11cb83546cf1376013f8512f025ef7d09
Sergey Volkov <js2me@outlook.com>
2025-01-06 10:47:34 +0300

    fix: try to fix loading json data

--- src/pages/[code]/ui/view.tsx
+++ src/pages/[code]/ui/view.tsx
@@ -10,26 +10,32 @@ export const CodePageView = observer(
     return (
       <Layout>
         <div className={'flex flex-col gap-3'}>
-          <h1 className={'prose text-8xl font-mono font-semibold leading-none'}>
+          <h1
+            className={
+              'prose text-9xl font-mono font-semibold leading-none tracking-wider -mb-4'
+            }
+          >
             {model.data?.code}
           </h1>
-          <h2 className={'prose text-2xl'}>{model.data?.title}</h2>
-          <p className={'prose text-lg'}>{model.data?.description}</p>
-          <p className={'prose text-lg'}>Источники:</p>
-          {model.data?.links.length ? (
-            model.data.links.map((link) => (
-              <a
-                referrerPolicy={'no-referrer'}
-                href={link}
-                key={link}
-                className={'link link-info'}
-              >
-                {link}
-              </a>
-            ))
-          ) : (
-            <span className={'prose text-sm opacity-80'}>Нет источников</span>
-          )}
+          <h2 className={'prose text-5xl mb-2'}>{model.data?.title}</h2>
+          <p className={'prose text-xl'}>{model.data?.description}</p>
+          <section className={'flex flex-col'}>
+            <p className={'prose text-lg opacity-80'}>Источники:</p>
+            {model.data?.links.length ? (
+              model.data.links.map((link) => (
+                <a
+                  referrerPolicy={'no-referrer'}
+                  href={link}
+                  key={link}
+                  className={'link link-info'}
+                >
+                  {link}
+                </a>
+              ))
+            ) : (
+              <span className={'prose text-sm opacity-80'}>Нет источников</span>
+            )}
+          </section>
           <div className={'flex flex-row flex-wrap gap-4 pt-4'}>
             {model.data?.images.length
               ? model.data?.images.map((image) => {
